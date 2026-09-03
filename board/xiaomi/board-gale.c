03326df04be70bc14e407294e6958dc5c526a780
Akamiya Chizui <127862333+Chizuui@users.noreply.github.com>
2026-09-03 01:51:09 +0000

    feat : add support for Redmi 13C ( gale )

---
+++ board/xiaomi/board-gale.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2026 Akamiya Chizui <127862333+Chizuui@users.noreply.github.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Redmi 13C\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Redmi 13C\n");
+}
